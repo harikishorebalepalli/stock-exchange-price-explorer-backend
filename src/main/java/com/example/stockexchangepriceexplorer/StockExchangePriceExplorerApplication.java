38c90e693b425fd5601f4e19112a8f83a653c761
Hari kishore <harikishore_balepalli@comcast.com>
2022-09-23 16:26:09 +0530

    init commit

---
+++ src/main/java/com/example/stockexchangepriceexplorer/StockExchangePriceExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockexchangepriceexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockExchangePriceExplorerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StockExchangePriceExplorerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/stockexchangepriceexplorer/StockExchangePriceExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockexchangepriceexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockExchangePriceExplorerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
